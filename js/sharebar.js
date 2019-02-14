2efb0fe2e25d5b7621573169cb738ae048f112d3
jacob-ressler <jressler96@gmail.com>
2019-02-14 18:57:01 -0500

    moved sharebar script to its own file

--- js/sharebar.js
+++ js/sharebar.js
@@ -3,13 +3,14 @@
 $(document).ready(function() {
   // Get the current URL and remove any section IDs from the end of it.
   let shareurl = location.href;
-  if (shareurl.includes("#")) shareurl.slice(0, location.href.indexOf("#"));
+  if (shareurl.includes("#"))
+    shareurl = shareurl.slice(0, location.href.indexOf("#"));
 
   // Now just replace any "$$$" with shareurl for each share-link element.
   let links = $(".share-link");
 
   for (let i = 0; i < links.length; i++) {
     links[i].href = links[i].href.replace("{$}", shareurl);
-    console.log(links[i].href);
+    //console.log(links[i].href);
   }
 });
